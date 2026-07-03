1d6b7a170b0f05dd7cc86001b888fb26440a1253
Felipe Cruz <143958236+felipejscruz@users.noreply.github.com>
2026-07-03 09:59:02 -0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+export const supabase = createClient(
+  "https://tyjwvpuyqztslrnloujj.supabase.co/rest/v1/",
+  "sb_publishable_4mJ9wxCmf_dPwK_ejkM04Q_cUDVwrEV"
+);
