edd37c0752b7dc7cfdb936e75fdd7b230f4b55ec
Felipe Cruz <143958236+felipejscruz@users.noreply.github.com>
2026-07-03 10:04:37 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,60 +1,22 @@
-const form =
-document.getElementById("loginForm");
+import { supabase } from "./supabase.js";
 
-form.addEventListener("submit", async (e)=>{
+const form = document.getElementById("formLogin");
 
+form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  const cpf =
-  document.getElementById("cpfLogin").value;
+  const email = document.getElementById("email").value;
+  const senha = document.getElementById("senha").value;
 
-  const senha =
-  document.getElementById("senhaLogin").value;
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email: email,
+    password: senha
+  });
 
-  const url =
-  "https://script.google.com/macros/s/AKfycbyN3es_E3iqB2eNCI6Sol6G-i54cAricV6dgUR0nGjZiyZ6D0GFyQfaidG2fZUY6G5ixA/exec"
-  + "?acao=login"
-  + "&cpf=" + cpf
-  + "&senha=" + senha;
-
-  try{
-
-    const resposta =
-    await fetch(url);
-
-    const resultado =
-    await resposta.json();
-
-    console.log(resultado);
-
-    if(resultado.status == "sucesso"){
-
-      localStorage.setItem(
-        "usuario",
-        JSON.stringify(resultado.usuario)
-      );
-
-      alert("Login realizado!");
-
-      window.location.href =
-      "painel.html";
-
-    }
-
-    else{
-
-      alert("CPF ou senha inválidos");
-
-    }
-
+  if (error) {
+    alert("Erro no login: " + error.message);
+  } else {
+    alert("Login realizado com sucesso!");
+    window.location.href = "index.html";
   }
-
-  catch(erro){
-
-    console.log(erro);
-
-    alert("Erro ao conectar");
-
-  }
-
 });
