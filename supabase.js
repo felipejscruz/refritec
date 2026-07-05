acef5ef73b316e01911b24b1a25345e3a1b7cb7f
Felipe Cruz <143958236+felipejscruz@users.noreply.github.com>
2026-07-05 20:44:09 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const supabaseUrl = "https://tyjwvpuyqztslrnloujj.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5and2cHV5cXp0c2xybmxvdWpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwNzAyMTMsImV4cCI6MjA5ODY0NjIxM30.8HEhgO68j9zb_xQ-VSOYmwzNS6uyb0ZTf1v4Wy_yZW4";
+const supabaseAnonKey = "sb_publishable_4mJ9wxCmf_dPwK_ejkM04Q_cUDVwrEV";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
